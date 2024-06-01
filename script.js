b453d0a2465714ce01c0b3d72e345b1a9f96235c
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-02 00:07:13 +0800

    finished with main assignment

--- script.js
+++ script.js
@@ -20,11 +20,96 @@ const createLinkedList = function createLinkedList() {
     }
   };
 
-  const toString = function toString() {
-    if (head === null) return null;
+  const prepend = function prepend(value) {
+    const newhead = createNode(value, head);
+    head = newhead;
+  };
+
+  const size = function size() {
+    if (head === null) return 0;
+    else {
+      let count = 1;
+      let currentNode = head;
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+        count++;
+      }
+      return count;
+    }
+  };
+
+  const getHead = function getHead() {
+    return head;
+  };
+
+  const getTail = function getTail() {
+    if (head === null) return head;
     else {
       let currentNode = head;
-      let string  = currentNode.value + " -> ";
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+      return currentNode;
+    }
+  };
+
+  const at = function at(index) {
+    if (head !== null) {
+      let currentNode = head;
+      while (index > 0 && currentNode !== null) {
+        currentNode = currentNode.nextNode;
+        index--;
+      }
+      if (currentNode !== null) {
+        return currentNode.value;
+      }
+    }
+  };
+
+  const pop = function pop() {
+    if (head !== null) {
+      if (head.nextNode === null) head = null;
+      else {
+        let previousNode;
+        let currentNode = head;
+        while (currentNode.nextNode !== null) {
+          previousNode = currentNode;
+          currentNode = currentNode.nextNode;
+        }
+        previousNode.nextNode = null;
+      }
+    }
+  };
+
+  const contains = function contains(value) {
+    if (head !== null) {
+      let currentNode = head;
+      while (currentNode !== null) {
+        if (currentNode.value == value) return true;
+        currentNode = currentNode.nextNode;
+      }
+    }
+    return false;
+  };
+
+  const find = function find(value) {
+    if (head !== null) {
+      let count = 0;
+      let currentNode = head;
+      while (currentNode !== null) {
+        if (currentNode.value == value) return count;
+        currentNode = currentNode.nextNode;
+        count++;
+      }
+    }
+    return null;
+  };
+
+  const toString = function toString() {
+    if (head === null) return "null";
+    else {
+      let currentNode = head;
+      let string = currentNode.value + " -> ";
       while (currentNode.nextNode !== null) {
         currentNode = currentNode.nextNode;
         string = string + currentNode.value + " -> ";
@@ -33,5 +118,48 @@ const createLinkedList = function createLinkedList() {
     }
   };
 
-  return { append, toString };
+  return {
+    append,
+    prepend,
+    toString,
+    size,
+    getHead,
+    getTail,
+    at,
+    pop,
+    contains,
+    find,
+  };
 };
+
+//tests
+
+const list = createLinkedList();
+list.prepend("banana");
+list.append("carrot");
+list.prepend("apple");
+list.append("durian");
+
+const toStringCheck =
+  list.toString() == "apple -> banana -> carrot -> durian -> null";
+const sizeCheck = list.size() == 4;
+const headCheck = list.getHead().value == "apple";
+const tailCheck = list.getTail().value == "durian";
+const atCheck = list.at(1) == "banana" && list.at(3) == "durian";
+const containsCheck = list.contains("banana") && !list.contains("lemon");
+const findCheck = list.find("carrot") == 2 && list.find("lemon") == null;
+
+console.log("toString: " + toStringCheck);
+console.log("size: " + sizeCheck);
+console.log("head: " + headCheck);
+console.log("tail: " + tailCheck);
+console.log("at: " + atCheck);
+console.log("contains: " + containsCheck);
+console.log("find :" + findCheck);
+
+list.pop();
+const popCheck1 = list.toString() == "apple -> banana -> carrot -> null";
+list.pop();
+const popCheck2 = list.toString() == "apple -> banana -> null";
+const popCheck = popCheck1 && popCheck2;
+console.log("pop: " + popCheck);
